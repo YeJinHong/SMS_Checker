8b9c27604236a2a0914814dc4374fdddd06c67bc
HongYe-jin <teepij@naver.com>
2021-01-22 23:05:55 +0900

    Message값을 저장하는 클래스 Message.java추가. 현 프로젝트는 특정 번호에 대해서만 처리를 하기때문에 위는 삭제될 수 있음. MainActivity에 메시지값을 읽어오는 함수 readSMSMessage추가 전체 문자메시지를 읽어와 Message 클래스에 저장하는 듯함. 불필요한 코드를 제거하는 과정이 필요하다.

--- app/src/main/java/com/example/hello3/MainActivity.java
+++ app/src/main/java/com/example/hello3/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hello3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,6 +16,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Message msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +38,42 @@ public class MainActivity extends AppCompatActivity {
         else {
 
         }
+    }
+
+    //SMS문자 메시지를 읽어오는 코드. 전체 메시지를 읽어오는 것으로 추정되며, 특정 번호에 대한 처리가 필요함.
+    public int readSMSMessage(){
+        Uri allMessage = Uri.parse("content://sms");
+        ContentResolver cr = getContentResolver();
+        Cursor c = cr.query(allMessage,
+                new String[]{"_id", "thread_id", "address", "person", "date", "body"},
+                null, null, "date DESC");
+
+        while(c.moveToNext()){
+            msg = new Message();
+
+            long messageId = c.getLong(0);
+            msg.setMessageId(String.valueOf(messageId));
+
+            long threadId = c.getLong(1);
+            msg.setThreadId(String.valueOf(threadId));
+
+            String address = c.getString(2);
+            msg.setAddress(address);
+
+            long contactId = c.getLong(3);
+            msg.setContactId(String.valueOf(contactId));
+
+            String contactId_string = String.valueOf(contactId);
+            msg.setContactId_string(contactId_string);
+
+            long timestamp = c.getLong(4);
+            msg.setTimestamp(String.valueOf(timestamp));
+
+            String body = c.getString(5);
+            msg.setBody(body);
+
+        }
+        return 0;
     }
 
 }
